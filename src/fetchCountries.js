9196f5e9632b1b065b2788549bae9dc02d1c3901
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-03-27 02:55:33 +0300

    fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,11 +2,11 @@ const BASE_URL = 'https://restcountries.com/v3.1/';
 const options = 'fields=name,capital,population,flags,languages';
 
 export default function fetchCountries(name) {
-  return fetch(`${BASE_URL}name/${name}?${options}`).then(res => {
-    if (!res.ok) {
+  return fetch(`${BASE_URL}name/${name}?${options}`).then(response => {
+    if (!response.ok) {
       throw new Error(response.status);
     }
 
-    return res.json();
+    return response.json();
   });
 }
