be201085d3cd9a137422029e0bf451da9bb70820
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-03-27 23:31:29 +0300

    fix bags

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,16 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import './css/styles.css';
+import '../css/styles.css';
 import fetchCountries from './fetchCountries';
-import countryCardTemplate from './country-card.hbs';
-import countryListTemplate from './country-list.hbs';
+import { makeCountryListMarkup, makeCountryCardMarkup } from './renderMarkup';
 
 const debounce = require('lodash.debounce');
-
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box');
 const countryBox = document.querySelector('.country-info');
 const countryList = document.querySelector('.country-list');
+let countriesArray = [];
 let selectedCountry = null;
-
-countryBox.classList.add('hidden');
 
 searchBox.addEventListener('input', debounce(onSearchBoxInput, DEBOUNCE_DELAY));
 
@@ -21,54 +18,44 @@ function onSearchBoxInput(e) {
   const countryName = e.target.value.trim();
   const countriesArray = [];
 
-  if (countryName === '') {
+  if (!countryName) {
     cleanMarkup(countryBox, countryList);
     countryBox.classList.add('hidden');
     return;
   }
-
   fetchCountries(countryName)
     .then(countryObj => {
       countryObj.map(country => {
         selectedCountry = country;
         countriesArray.push(country);
       });
-      if (countriesArray.length > 10) {
-        Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-        return;
-      } 
+
       generateMarkup(countriesArray, countryList, countryBox);
     })
     .catch(err => {
-      console.log(err, err.stack)
+      console.log(err, err.stack);
       Notify.failure('Oops, there is no country with that name');
       countryBox.classList.add('hidden');
       cleanMarkup(countryBox, countryList);
     });
 }
-
 function generateMarkup(countriesArray, countryList, countryBox) {
-  if (countriesArray.length >= 2 && countriesArray.length <= 10) {
+  if (countriesArray.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  } else if (countriesArray.length >= 2 && countriesArray.length <= 10) {
     countryBox.classList.remove('hidden');
-    countryList.innerHTML = countryListTemplate(countriesArray);
+    countryList.innerHTML = makeCountryListMarkup(countriesArray);
     countryBox.classList.add('hidden');
   } else {
     countryBox.classList.remove('hidden');
     countryList.innerHTML = '';
-    // changeCountryLang(selectedCountry);
+    console.log(selectedCountry);
     renderCountryCard(selectedCountry);
   }
 }
-
-function changeCountryLang(country) {
-  const languages = Object.values(country.languages).join(', ');
-  const countryWithLanguages = { ...country, languages: languages };
-}
-
 function renderCountryCard(country) {
-  countryBox.innerHTML = countryCardTemplate(country);
+  countryBox.innerHTML = makeCountryCardMarkup(country);
 }
 function cleanMarkup(countryBox, countryList) {
   countryList.innerHTML = '';
---
+++ src/js/renderMarkup.js
@@ -0,0 +1,32 @@
+function makeCountryListMarkup(country) {
+  return country
+    .map(country => {
+      return `<li class="country-list__item">
+        <img class="country-list__img" src="${country.flags.svg}" alt="" width="30" height="20">
+        <p>${country.name.official}</p>
+    </li>`;
+    })
+    .join('');
+}
+function makeCountryCardMarkup(country) {
+  return `<div class="country-name">
+        <img class="flag" src
+        ="${country.flags.svg}" alt="${
+    country.name.common
+  }" width="50" height="50">
+        <h2>${country.name.official}</h2>
+        </div>
+        <p class="capital">Capital: <span class="card-accent">${
+          country.capital
+        }</span></p>
+        <p class="population">Population: <span class="card-accent">${
+          country.population
+        }</span></p>
+        <div class="languages-box">
+        <p>Languages:</p>
+        <p class="list-group-item"><span class="card-accent">${Object.values(
+          country.languages
+        ).join(', ')}</span></p>
+    </div>`;
+}
+export { makeCountryListMarkup, makeCountryCardMarkup };
